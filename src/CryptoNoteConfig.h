22db81d7c6e477eab60a8df63f7e95ee16323e35
lysergicamine <33916663+lysergicamine@users.noreply.github.com>
2017-12-04 05:27:38 -0330

    Update CryptoNoteConfig.h
    seed ip may need adjusting.

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -106,6 +106,8 @@ const char     P2P_STAT_TRUSTED_PUB_KEY[]                    = "8f80f9a5a434a9f1
 
 //TODO Add here your network seed nodes
 const std::initializer_list<const char*> SEED_NODES = {
+  "192.168.1.11:48620",
+  "192.168.1.12:48620",
   //"your_seed_ip1.com:8080",
   //"your_seed_ip2.com:8080",
 };
